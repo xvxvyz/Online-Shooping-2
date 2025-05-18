27d818f7d76a0808b0ec19cabaa55d80279c59ce
Celvin Saputra <celvingaming12@gmail.com>
2025-05-19 00:51:49 +0700

    Update Electronics.java

--- Electronics.java
+++ Electronics.java
@@ -1,72 +1,42 @@
-// Concrete class Electronics (turunan dari Product dan mengimplementasikan Discountable)
-class Electronics extends Product implements Discountable {
-    private String brand;
-    private String model;
-    private Specification specs; // Instance dari inner class Specification
+public class Electronics extends Product implements Discountable {
+    private Specification spec;
 
-    // Inner class Specification
-    public class Specification {
-        private double weight;
-        private String deviceBrand;
-
-        public Specification(double weight, String deviceBrand) {
-            this.weight = weight;
-            this.deviceBrand = deviceBrand;
-        }
-
-        public double getWeight() {
-            return weight;
-        }
-
-        public String getDeviceBrand() {
-            return deviceBrand;
-        }
-
-        public String getSpecs() {
-            return "Berat: " + weight + " kg, Merek Perangkat: " + deviceBrand;
-        }
-    }
-
-    public Electronics(String name, double price, String brand, String model, double weight) {
+    public Electronics(String name, double price, String brand, double weight) {
         super(name, price);
-        this.brand = brand;
-        this.model = model;
-        this.specs = new Specification(weight, brand); // Membuat instance Specification saat Electronics dibuat
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public Specification getSpecs() {
-        return specs;
+        this.spec = new Specification(weight, brand);
     }
 
     @Override
     public String getCategory() {
-        return "Elektronik";
-    }
-
-    @Override
-    public void displayDetails() {
-        super.displayDetails();
-        System.out.println("Kategori: " + getCategory());
-        System.out.println("Merek: " + brand);
-        System.out.println("Model: " + model);
-        System.out.println("Spesifikasi: " + specs.getSpecs()); // Menampilkan spesifikasi
+        return "Electronics";
     }
 
     @Override
     public double getDiscountedPrice(double discountPercentage) {
-        if (discountPercentage < 0 || discountPercentage > 100) {
-            System.out.println("Persentase diskon tidak valid.");
-            return getPrice();
+        return price - (price * discountPercentage / 100);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Nama Produk: " + name);
+        System.out.println("Kategori: " + getCategory());
+        System.out.println("Harga: Rp" + price);
+        System.out.println("Spesifikasi: " + spec.getSpecs());
+        System.out.println("Tanggal Dibuat: " + createdAt);
+    }
+
+    // Inner Class
+    public class Specification {
+        private double weight;
+        private String brand;
+
+        public Specification(double weight, String brand) {
+            this.weight = weight;
+            this.brand = brand;
         }
-        double discountAmount = getPrice() * (discountPercentage / 100);
-        return getPrice() - discountAmount;
+
+        public String getSpecs() {
+            return brand + ", " + weight + " kg";
+        }
     }
 }
